1df496649539733d21459c64f7d8a0523b247335
mjjonone <142796823+mjjonone@users.noreply.github.com>
2024-03-07 11:09:03 +0800

    Fix ua recognition errors

--- _worker.js
+++ _worker.js
@@ -1,4 +1,3 @@
-// Define constants including token, main data, subscription URLs, subscription backend converter, and subscription configuration file
 const mytoken = '123'; 
 const MainData = ``;
 const urls = ['https://allsub.king361.cf'];
@@ -6,33 +5,28 @@ const subconverter = "back.889876.xyz";
 const subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"; 
 
 export default {
-	// Define an asynchronous fetch function to handle requests
 	async fetch (request) {
-		// Get the User-Agent information from the request header and convert it to lowercase
 		const userAgent = (request.headers.get('User-Agent') || "null").toLowerCase();
-		
-		// Get the token from the request URL
 		const url = new URL(request.url);
 		const token = url.searchParams.get('token');
 
-		// Check if the token is valid
+		// Token validation
 		if (!(token == mytoken || url.pathname.includes("/"+ mytoken))) {
 			return new Response('Invalid token???', { status: 403 });
 		}
 
-		// Initialize the request data and add the main data
 		let req_data = MainData;
 
-		// Parallelly get the responses from all subscription URLs
+		// Fetch responses from all subscription URLs concurrently
 		const responses = await Promise.all(urls.map(url => fetch(url,{
 			method: 'get',
 			headers: {
 				'Accept': 'text/html,application/xhtml+xml,application/xml;',
-				'User-Agent': 'Mozilla/5.0 Chrome/90.0.4430.72'
+				'User-Agent': 'worker/sub/mjjonone'
 			}
 		})));
 
-		// Iterate through the responses, if the response is normal, decode the content and add it to the request data
+		// Add content of successful responses to request data
 		for (const response of responses) {
 			if (response.ok) {
 				const content = await response.text();
@@ -40,35 +34,45 @@ export default {
 			}
 		}
 
-		// Check if the User-Agent contains specific targets, if so, get the converted subscription content and return
-		const targets = ['clash', 'sing-box', 'singbox', 'Shadowrocket', 'Quantumult'];
-		for (const target of targets) {
-			if (userAgent.includes(target)) {
-				const subTarget = ['clash', 'Shadowrocket', 'Quantumult'].includes(target) ? 'clash' : 'singbox';
-				const subconverterUrl = `https://${subconverter}/sub?target=${subTarget}&url=${encodeURIComponent(request.url)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&new_name=true`;
-
-				try {
-					const subconverterResponse = await fetch(subconverterUrl);
-					
-					if (!subconverterResponse.ok) {
-						throw new Error(`Error fetching subconverterUrl: ${subconverterResponse.status} ${subconverterResponse.statusText}`);
-					}
-					
-					const subconverterContent = await subconverterResponse.text();
-					
-					return new Response(subconverterContent, {
-						headers: { 'content-type': 'text/plain; charset=utf-8' },
-					});
-				} catch (error) {
-					return new Response(`Error: ${error.message}`, {
-						status: 500,
-						headers: { 'content-type': 'text/plain; charset=utf-8' },
-					});
-				}
-			}
+		const TARGETS = ['clash', 'sing-box', 'singbox', 'shadowrocket', 'quantumult'];
+		const SUBTARGET_MAP = new Map([
+		  ['clash', 'clash'],
+		  ['sing-box', 'singbox'],
+		  ['singbox', 'singbox'],
+		  ['shadowrocket', 'clash'],
+		  ['quantumult', 'clash']
+		]);
+		
+		// Function to fetch converted subscription content
+		async function fetchSubscriptionContent(target, request, subconverter, subconfig) {
+		  const subTarget = SUBTARGET_MAP.get(target);
+		  const subconverterUrl = `https://${subconverter}/sub?target=${subTarget}&url=${encodeURIComponent(request.url)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=false&fdn=false&sort=false&new_name=true`;
+		
+		  const subconverterResponse = await fetch(subconverterUrl);
+		
+		  if (!subconverterResponse.ok) {
+			throw new Error(`Error fetching subconverterUrl: ${subconverterResponse.status} ${subconverterResponse.statusText}`);
+		  }
+		
+		  return await subconverterResponse.text();
 		}
+		
+		// Fetch converted subscription content if User-Agent matches any target
+		for (const target of TARGETS) {
+		  if (userAgent.toLowerCase().includes(target)) {
+			try {
+			  const subconverterContent = await fetchSubscriptionContent(target, request, subconverter, subconfig);
+			  
+			  return new Response(subconverterContent, {
+				headers: { 'content-type': 'text/plain; charset=utf-8' },
+			  });
+			} catch (error) {
+			  console.error(error);
+			}
+		  }
+		}		
 
-		// If no User-Agent matches, encode the request data to base64 and return
+		// Encode request data to base64 if no User-Agent matches
 		const utf8Encoder = new TextEncoder();
 		const encodedData = utf8Encoder.encode(req_data);
 		const base64Data = btoa(String.fromCharCode.apply(null, encodedData));
